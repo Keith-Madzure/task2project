34b7cb151381b08387b06fa4f71fe8df068b211a
Keith-Madzure <madzurekeith@gmail.com>
2026-05-23 16:18:24 +0200

    Project 2 commit

---
+++ src/main/java/pl/edu/vistula/firstrestapispring/product/api/ProductController.java
@@ -0,0 +1,39 @@
+package pl.edu.vistula.firstrestapispring.product.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.edu.vistula.firstrestapispring.product.api.request.ProductRequest;
+import pl.edu.vistula.firstrestapispring.product.api.response.ProductResponse;
+import pl.edu.vistula.firstrestapispring.product.service.ProductService;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Create product")
+    public ResponseEntity<ProductResponse> create(@RequestBody ProductRequest productRequest) {
+        ProductResponse productResponse = productService.create(productRequest);
+        return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Find product by id")
+    public ResponseEntity<ProductResponse> find(@PathVariable Long id) {
+        ProductResponse productResponse = productService.find(id);
+        return ResponseEntity.status(HttpStatus.OK).body(productResponse);
+    }
+}
